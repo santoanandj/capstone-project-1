dcb05ac9e13714c690fbb2658a70c32e60c892fd
Ubuntu <ubuntu@ip-172-31-10-103.ap-south-1.compute.internal>
2023-03-09 19:46:43 +0000

    3

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-ls 
-#making react dirctory
-mkdir react
-#copying all file into directory
-cp -f * react
-#taring
-tar -cvzf react.tar.gz react
-#copying to deploy server
-scp -o StrictHostKeyChecking=no -i $key react.tar.gz ubuntu@3.110.182.16:/home/ubuntu
-ssh -T -o StrictHostKeyChecking=no -i $key ubuntu@3.110.182.16<<EOF
-cd /home/ubuntu
 rm -rf react || true
 sudo docker system prune -y
 #untaring
