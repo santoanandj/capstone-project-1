6c3d73aff94495d730571e30a0d024b8582eeff8
Ubuntu <ubuntu@ip-172-31-10-103.ap-south-1.compute.internal>
2023-03-09 19:07:13 +0000

    2nd commit

---
+++ gitbranch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [[ $GIT_BRANCH == origin/dev ]]
+then
+	chmod +x deploy.sh
+	./deploy.sh
+	docker tag react:v1 santoanandj/dev:v1
+        docker push santoanandj/dev:v1
+        #dockerhub push
+	docker login --username=$docker_username --password=$docker_password
+	echo $docker_password | docker login -u $docker_username --password-stdin
+elif [[ $GIT_BRANCH == origin/main ]]
+then
+	chmod +x deploy.sh
+	./deploy.sh
+	docker tag react:v1 santoanandj/prod:v1
+        docker push santoanandj/prod:v1
+	#dockerhub push
+        docker login --username=$docker_username --password=$docker_password
+        echo $docker_password | docker login -u $docker_username --password-stdin
+else
+	echo "deployment is failure"
+fi
